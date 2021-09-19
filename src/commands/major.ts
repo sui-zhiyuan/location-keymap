e6dc743b70244e9208a519ebedaad70c3c31eae9
Zhiyuan Sui <zhiyuansui@microsoft.com>
2021-09-19 10:52:58 +0800

    f1

--- src/commands/major.ts
+++ src/commands/major.ts
@@ -1,12 +1,12 @@
 import * as vscode from "vscode";
 
-import { cursorDownParagraph , cursorUpParagraph } from "./cursorMoveParagraph";
+import { cursorDownParagraph, cursorUpParagraph } from "./cursorMoveParagraph";
 
-let allCommands = [cursorDownParagraph , cursorUpParagraph]
+let allCommands = [cursorDownParagraph, cursorUpParagraph]
 
-export function registerCommands(context: vscode.ExtensionContext){
-    for(let command of allCommands){
-        let dispose = vscode.commands.registerTextEditorCommand(command.name , command.callback , command.thisArg)
+export function registerCommands(context: vscode.ExtensionContext) {
+    for (let command of allCommands) {
+        let dispose = vscode.commands.registerTextEditorCommand(command.name, command.callback, command.thisArg)
         context.subscriptions.push(dispose)
     }
 }
